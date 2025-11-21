5cf21c8fe51011c9e09ce3b6e4bd0ac31db2d140
Ricky <ricky.martin2004m@gmail.com>
2025-11-21 21:28:10 +0000

    Viewing My Dataset and Checking my Columns for The project

--- main.py
+++ main.py
@@ -2,3 +2,15 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+
+stores_dataset = pd.read_csv("stores data-set (1).csv")
+print(stores_dataset)
+print("Stores Dataset Columns: ", stores_dataset.columns)
+
+sales_dataset = pd.read_csv("sales data-set.csv")
+print(sales_dataset)
+print("Sales Dataset Columns: ", sales_dataset.columns)
+
+features_dataset = pd.read_csv("Features data set.csv")
+print(features_dataset)
+print("Features Dataset Columns: ", features_dataset.columns)
