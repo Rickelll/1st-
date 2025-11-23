a733170f4b57e689e7cf30c514ad0a202bcc09ab
Ricky <ricky.martin2004m@gmail.com>
2025-11-23 19:35:15 +0000

    Average of Sales with Holidays vs No Holidays

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+#STEP - 1
+#Cleaning the Dataset
+
 stores_dataset = pd.read_csv("stores data-set (1).csv")
 print(stores_dataset)
 print("Stores Dataset Columns: ", stores_dataset.columns)
@@ -70,3 +73,10 @@ print(dataset.head())
 print(dataset.isnull().sum())
 print("Dataset Columns: ", dataset.columns)
 print(dataset.head())
+
+#STEP - 2
+#Finding the sales Difference between holidays and non-holidays weeks
+avg_weekly_sales_no_holidays = dataset[dataset['IsHoliday'] == 0]['Weekly_Sales'].mean()                    #0 is false so they don't have a holiday
+print(f"Averager With No Holidays (Weekly Sales): {avg_weekly_sales_no_holidays}")
+avg_weekly_sales_with_holidays = dataset[dataset['IsHoliday'] == 1]['Weekly_Sales'].mean()                  #1 is True so they do have a holiday
+print(f"Average With Holidays (Weekly Sales): {avg_weekly_sales_with_holidays}")
